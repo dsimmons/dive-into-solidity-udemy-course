dc48e2d14027a6eb1c233e96124b185fd168095a
Dan Simmons <dan@simmons.io>
2022-11-27 20:52:27 -0700

    Passed knowledge test challenges!

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -5,9 +5,36 @@ pragma solidity >=0.5.0 <0.9.0;
 contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
+    address public owner;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
-        t[0] = "VET";
+    constructor() {
+      owner = msg.sender;
     }
+
+    function changeTokens() public {
+      tokens[0] = "VET";
+    }
+
+    function getBalance() public view returns (uint) {
+      return address(this).balance;
+    }
+
+    function transferAll(address _addr) external {
+      require(msg.sender == owner, "ONLY_OWNER");
+      (bool isSuccess,) = _addr.call{value: address(this).balance}("");
+      require (isSuccess, "Transaction not successful!");
+    }
+
+    function start() external {
+      players.push(msg.sender);
+    }
+
+    function concatenate(string memory str1, string memory str2)
+    external
+    pure
+    returns (string memory) {
+      return string(abi.encodePacked(str1, str2));
+    }
+
+    receive() external payable {}
 }
